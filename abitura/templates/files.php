7bd7703d54c718f28bcaf0686069e9612fdeeb11
Ruslan26091994 <r.shapovalov94@gmail.com>
2016-11-18 20:34:38 +0300

    commit "abitura"

---
+++ abitura/templates/files.php
@@ -0,0 +1,37 @@
+<?php
+	require_once "/lang/lang1.php";
+	
+?>
+<head>
+	<title><?php echo $Lang['files']; ?></title>
+	<meta http-equiv="Content-type" content="text/html; charset=Windows-1251" />
+        <link href="../static/css/css_menu/style.css" rel="stylesheet" type="text/css" />
+		<link href="../static/css/css_menu/style1.css" rel="stylesheet" type="text/css" />
+		<link href="../static/css/css_menu/menu.css" rel="stylesheet" type="text/css" />
+		<link rel="stylesheet" type="text/css" href="../static/css/css_menu/component.css" />
+		<link href="https://fonts.googleapis.com/css?family=Bree+Serif|Dancing+Script|Philosopher" rel="stylesheet">
+		<link rel="shortcut icon" href="../static/images/images/icon.jpg" type="image/jpg">
+</head>
+
+<body>
+<div class="wrapper">
+	<table width="100%">
+		<header class="header">
+		<?php
+			require_once "../static/blocks/top7.php";
+		?>
+	</table>
+<div id="header"></div>
+	</header>
+	<main class="content">
+	<?php
+		require_once "../static/blocks/content_files.php";
+	?>
+	</main>
+</div>
+	<footer class="footer">
+	<?php
+		require_once "../static/blocks/footer.php";
+	?>
+	</footer>
+</body>
