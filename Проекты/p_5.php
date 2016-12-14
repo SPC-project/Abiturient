73cea4ef6da54047f8eb7535ba722181690d5da3
Alexander Vasilchuk <aectann101@gmail.com>
2016-12-14 22:50:21 +0200

    Adding files to repository

---
+++ Проекты/p_5.php
@@ -0,0 +1,19 @@
+﻿<div class="container"style="margin-bottom: 20px;">
+    <div class="row">
+      	<div class="col-md-10 col-md-offset-2">
+      		
+			<center><h3><a href="p_1.php" target="_self">Разработка программы для анализа и прогноза скорости и направления ветра</a></h3><br></center>				 
+							
+				<big><p align="justify" style="text-indent:25px">
+				Создана программа оценки воздушных потоков в данной местности на  основании данных синоптических сайтов. Данная задача решалась следующим образом:<br></p>
+<p align="justify" style="text-indent:25px">• Реализован анализ ветра, а именно средних скоростей, преобладающих направлений ветра в выбранной местности.<br></p>
+<p align="justify" style="text-indent:25px">• Спрогнозированы возможные скорость и направления ветра в ближайшие сутки.<br></p>
+<p align="justify" style="text-indent:25px">• Данные для прогноза накапливаются из сайта глобальной метеослужбы  http://ready.arl.noaa.gov для с. Долгенькое Изюмского района. <br></p>
+<p align="justify" style="text-indent:25px">• Написан графический интерфейс пользователя на С++ с использованиембиблиотеки Qt.<br></p></big>
+<p><img src="<?=base_url();?>assets/images/rv1.jpg" width="950" height="300" style="float:center;"></p><br>
+<p><img src="<?=base_url();?>assets/images/rv2.jpg" width="950" height="500" style="float:center;"></p>
+				
+			
+		</div>
+	</div>
+</div>
